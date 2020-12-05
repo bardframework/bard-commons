f2f2f89b11e2fa78c6c04e22bc8dd8aafcbf4882
Vahid Zafari <va.zafari@gmail.com>
2020-12-05 16:23:32 +0330

    update swagger configs

--- common-swagger/src/main/java/org/bardframework/commons/swagger/SwaggerConfigurator.java
+++ common-swagger/src/main/java/org/bardframework/commons/swagger/SwaggerConfigurator.java
@@ -6,31 +6,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.AuthorizationScopeBuilder;
-import springfox.documentation.service.*;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static springfox.documentation.builders.PathSelectors.ant;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfigurator implements WebMvcConfigurer, Configurator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerConfigurator.class);
 
     @Bean
-    public Docket customImplementation() {
-        return (new Docket(DocumentationType.SPRING_WEB)).apiInfo(this.apiInfo()).directModelSubstitute(LocalDateTime.class, String.class).directModelSubstitute(LocalDate.class, String.class);
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     private ApiInfo apiInfo() {
@@ -39,45 +35,13 @@ public class SwaggerConfigurator implements WebMvcConfigurer, Configurator {
                 .description("API Documentation")
                 .termsOfServiceUrl("#")
                 .contact(new Contact("Vahid Zafari", "https://www.linkedin.com/in/vzafari", "v.zafari@chmail.ir"))
-                .version("1.0")
                 .build();
-    }
-
-    @Bean
-    SecurityContext securityContext() {
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = new AuthorizationScopeBuilder()
-                .scope("global")
-                .description("accessEverything")
-                .build();
-
-        SecurityReference securityReference = SecurityReference.builder()
-                .reference("Authorization")
-                .scopes(authorizationScopes)
-                .build();
-
-        return SecurityContext.builder()
-                .securityReferences(newArrayList(securityReference))
-                .forPaths(ant("/**"))
-                .build();
-    }
-
-    @Bean
-    SecurityScheme apiKey() {
-        return new ApiKey("Authorization", "Authorization", "header");
-    }
-
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/**").addResourceLocations("/WEB-INF/assets/");
-        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
     public void configure(HttpSecurity httpSecurity) {
         try {
-            httpSecurity.authorizeRequests().antMatchers("/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/v2/api-docs").permitAll();
+            httpSecurity.authorizeRequests().antMatchers("/swagger-resources/**", "/swagger-ui/**", "/v2/api-docs", "/v3/api-docs").permitAll();
         } catch (Exception e) {
             LOGGER.error("error configuring swagger", e);
         }
