ec6cd32f860f3033ee719df9ab06f5367b571c90
v.zafari <va.zafari@gmail.com>
2022-11-26 07:48:22 +0330

    log all configs

--- common-spring-boot/src/main/java/org/bardframework/commons/spring/boot/ConfigsConfiguration.java
+++ common-spring-boot/src/main/java/org/bardframework/commons/spring/boot/ConfigsConfiguration.java
@@ -22,56 +22,65 @@ public class ConfigsConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigsConfiguration.class);
     private static final String CLASS_PATH_KEY = "java.class.path";
     private static final String SEPARATOR_KEY = "path.separator";
-    private static final Set<String> SENSITIVE_KEYS = new HashSet<>(Arrays.asList("password", "credential", "secret"));
+    private static final Set<String> SENSITIVE_KEY_PARTS = new HashSet<>(Arrays.asList("password", "credential", "secret", "token"));
     private static final Set<String> NOT_LOG_KEYS = new HashSet<>(Collections.singletonList(CLASS_PATH_KEY));
 
     @Bean
-    static PropertySourcesPlaceholderConfigurer placeHolderConfigurer(Environment environment) throws IOException {
+    PropertySourcesPlaceholderConfigurer placeHolderConfigurer(Environment environment) throws IOException {
         PathMatchingResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
-        List<Resource> resources = new ArrayList<>(Arrays.asList(patternResolver.getResources("classpath*:**/**-config.properties")));
+        Set<Resource> resources = new HashSet<>();
+        resources.addAll(List.of(patternResolver.getResources("classpath*:**/**-config.properties")));
         resources.addAll(Arrays.asList(patternResolver.getResources("classpath*:**/**/application.properties")));
         for (String profile : environment.getActiveProfiles()) {
             resources.addAll(Arrays.asList(patternResolver.getResources("classpath*:**/**/application-" + profile + ".properties")));
         }
-        ConfigsConfiguration.logConfigs(environment, resources);
-        PropertySourcesPlaceholderConfigurer propertyConfigurer = new PropertySourcesPlaceholderConfigurer();
+        BardPropertySourcesPlaceholderConfigurer propertyConfigurer = new BardPropertySourcesPlaceholderConfigurer();
         propertyConfigurer.setLocations(resources.toArray(new Resource[0]));
+        this.logConfigs(environment, propertyConfigurer, resources);
         return propertyConfigurer;
     }
 
-    private static void logConfigs(Environment environment, List<Resource> resources) {
+    private void logConfigs(Environment environment, BardPropertySourcesPlaceholderConfigurer configurer, Set<Resource> resources) throws IOException {
         final MutablePropertySources sources = ((AbstractEnvironment) environment).getPropertySources();
-        StringBuilder configs = new StringBuilder();
-        ConfigsConfiguration.append(configs, "Active profiles", Arrays.toString(environment.getActiveProfiles()));
-        String classpath = environment.getProperty(CLASS_PATH_KEY);
-        String separator = environment.getProperty(SEPARATOR_KEY);
-        ConfigsConfiguration.append(configs, "Config Files", resources.stream().map(Object::toString).collect(Collectors.joining("\n\t")));
-        if (StringUtils.isNotBlank(classpath) && StringUtils.isNotBlank(separator)) {
-            ConfigsConfiguration.append(configs, CLASS_PATH_KEY, Arrays.stream(classpath.split(separator)).map(Object::toString).collect(Collectors.joining("\n\t")));
-        } else {
-            LOGGER.info("classpath[{}] or separator[{}] is not valid", classpath, separator);
-            ConfigsConfiguration.append(configs, CLASS_PATH_KEY, classpath);
-        }
-        ConfigsConfiguration.append(configs, "DefaultCharset", String.valueOf(CharsetUtils.getDefaultCharset()));
-        ConfigsConfiguration.append(configs, "DefaultLocale", String.valueOf(CharsetUtils.getDefaultLocale()));
-        ConfigsConfiguration.append(configs, "DefaultEncoding", String.valueOf(CharsetUtils.getDefaultEncoding()));
-
+        Map<String, String> configs = new HashMap<>();
         StreamSupport.stream(sources.spliterator(), false)
                 .filter(propertySource -> propertySource instanceof EnumerablePropertySource)
                 .map(propertySource -> ((EnumerablePropertySource<?>) propertySource).getPropertyNames())
                 .flatMap(Arrays::stream)
-                .forEach(property -> {
-                    if (NOT_LOG_KEYS.contains(property.toLowerCase())) {
-                        /*
-                            do nothing
-                         */
-                    } else if (SENSITIVE_KEYS.contains(property.toLowerCase())) {
-                        ConfigsConfiguration.append(configs, property, "*****");
-                    } else {
-                        ConfigsConfiguration.append(configs, property, environment.getProperty(property));
-                    }
-                });
-        LOGGER.info("\n\n====== configuration ======\n{}====== configuration ======\n", configs);
+                .forEach(property -> configs.put(property, environment.getProperty(property)));
+
+        for (Map.Entry<Object, Object> entry : configurer.mergeProperties().entrySet()) {
+            configs.put(entry.getKey().toString(), entry.getValue().toString());
+        }
+
+        StringBuilder aggregatedConfig = new StringBuilder();
+        ConfigsConfiguration.append(aggregatedConfig, "Active profiles", Arrays.toString(environment.getActiveProfiles()));
+        String classpath = environment.getProperty(CLASS_PATH_KEY);
+        String separator = environment.getProperty(SEPARATOR_KEY);
+        ConfigsConfiguration.append(aggregatedConfig, "Config Files", resources.stream().map(Object::toString).collect(Collectors.joining("\n\t")));
+        if (StringUtils.isNotBlank(classpath) && StringUtils.isNotBlank(separator)) {
+            ConfigsConfiguration.append(aggregatedConfig, CLASS_PATH_KEY, Arrays.stream(classpath.split(separator)).map(Object::toString).collect(Collectors.joining("\n\t")));
+        } else {
+            LOGGER.info("classpath[{}] or separator[{}] is not valid", classpath, separator);
+            ConfigsConfiguration.append(aggregatedConfig, CLASS_PATH_KEY, classpath);
+        }
+        ConfigsConfiguration.append(aggregatedConfig, "DefaultCharset", String.valueOf(CharsetUtils.getDefaultCharset()));
+        ConfigsConfiguration.append(aggregatedConfig, "DefaultLocale", String.valueOf(CharsetUtils.getDefaultLocale()));
+        ConfigsConfiguration.append(aggregatedConfig, "DefaultEncoding", String.valueOf(CharsetUtils.getDefaultEncoding()));
+
+        configs.keySet().stream().sorted().forEach(property -> {
+            if (this.getNotLogKeys().contains(property.toLowerCase())) {
+                /*
+                    do nothing
+                 */
+            } else if (this.getSensitiveKeyParts().stream().anyMatch(sensitiveKey -> property.toLowerCase().contains(sensitiveKey))) {
+                ConfigsConfiguration.append(aggregatedConfig, property, "*****");
+            } else {
+                ConfigsConfiguration.append(aggregatedConfig, property, configs.get(property));
+            }
+        });
+
+        LOGGER.info("\n\n====== configuration ======\n{}====== configuration ======\n", aggregatedConfig);
     }
 
     private static void append(StringBuilder configs, String key, String value) {
@@ -79,5 +88,20 @@ public class ConfigsConfiguration {
         configs.append(":\n\t");
         configs.append(value);
         configs.append("\n");
+    }
+
+    protected Set<String> getSensitiveKeyParts() {
+        return SENSITIVE_KEY_PARTS;
+    }
+
+    protected Set<String> getNotLogKeys() {
+        return NOT_LOG_KEYS;
+    }
+
+    private static class BardPropertySourcesPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
+        @Override
+        public Properties mergeProperties() throws IOException {
+            return super.mergeProperties();
+        }
     }
 }
